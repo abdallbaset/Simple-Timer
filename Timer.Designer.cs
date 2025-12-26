dfb02e2772b1ba5497b52f1b3e3e530f336b17ee
abdalbaset <abdallbaset2004@gmail.com>
2025-12-26 21:31:33 +0330

    feat: initial complete version of Simple Timer App

---
+++ Timer.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace Timer
+{
+    partial class Timer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lbl_Sacend = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.lbl_Minute = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.lbl_Hour = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbl_Timer = new System.Windows.Forms.Label();
+            this.btn_Start = new System.Windows.Forms.Button();
+            this.btn_Stop = new System.Windows.Forms.Button();
+            this.btn_Restart = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(35)))), ((int)(((byte)(50)))));
+            this.panel1.Controls.Add(this.lbl_Sacend);
+            this.panel1.Location = new System.Drawing.Point(508, 132);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(111, 90);
+            this.panel1.TabIndex = 0;
+            // 
+            // lbl_Sacend
+            // 
+            this.lbl_Sacend.Font = new System.Drawing.Font("Verdana", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Sacend.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lbl_Sacend.Location = new System.Drawing.Point(3, 16);
+            this.lbl_Sacend.Name = "lbl_Sacend";
+            this.lbl_Sacend.Size = new System.Drawing.Size(105, 59);
+            this.lbl_Sacend.TabIndex = 4;
+            this.lbl_Sacend.Text = "0";
+            this.lbl_Sacend.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(35)))), ((int)(((byte)(50)))));
+            this.panel2.Controls.Add(this.lbl_Minute);
+            this.panel2.Location = new System.Drawing.Point(360, 132);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(111, 90);
+            this.panel2.TabIndex = 1;
+            // 
+            // lbl_Minute
+            // 
+            this.lbl_Minute.Font = new System.Drawing.Font("Verdana", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Minute.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lbl_Minute.Location = new System.Drawing.Point(6, 16);
+            this.lbl_Minute.Name = "lbl_Minute";
+            this.lbl_Minute.Size = new System.Drawing.Size(102, 59);
+            this.lbl_Minute.TabIndex = 5;
+            this.lbl_Minute.Text = "0";
+            this.lbl_Minute.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(35)))), ((int)(((byte)(50)))));
+            this.panel3.Controls.Add(this.lbl_Hour);
+            this.panel3.Location = new System.Drawing.Point(209, 132);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(111, 90);
+            this.panel3.TabIndex = 1;
+            // 
+            // lbl_Hour
+            // 
+            this.lbl_Hour.Font = new System.Drawing.Font("Verdana", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Hour.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lbl_Hour.Location = new System.Drawing.Point(6, 16);
+            this.lbl_Hour.Name = "lbl_Hour";
+            this.lbl_Hour.Size = new System.Drawing.Size(102, 59);
+            this.lbl_Hour.TabIndex = 6;
+            this.lbl_Hour.Text = "0";
+            this.lbl_Hour.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Verdana", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(326, 148);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 59);
+            this.label1.TabIndex = 2;
+            this.label1.Text = ":";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Verdana", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(477, 148);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 59);
+            this.label2.TabIndex = 3;
+            this.label2.Text = ":";
+            // 
+            // lbl_Timer
+            // 
+            this.lbl_Timer.Font = new System.Drawing.Font("Verdana", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Timer.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lbl_Timer.Location = new System.Drawing.Point(326, 26);
+            this.lbl_Timer.Name = "lbl_Timer";
+            this.lbl_Timer.Size = new System.Drawing.Size(191, 59);
+            this.lbl_Timer.TabIndex = 6;
+            this.lbl_Timer.Text = "Timer";
+            this.lbl_Timer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btn_Start
+            // 
+            this.btn_Start.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(19)))), ((int)(((byte)(91)))), ((int)(((byte)(236)))));
+            this.btn_Start.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(19)))), ((int)(((byte)(91)))), ((int)(((byte)(236)))));
+            this.btn_Start.FlatAppearance.BorderSize = 0;
+            this.btn_Start.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DodgerBlue;
+            this.btn_Start.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Start.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Start.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn_Start.Location = new System.Drawing.Point(508, 318);
+            this.btn_Start.Name = "btn_Start";
+            this.btn_Start.Size = new System.Drawing.Size(117, 47);
+            this.btn_Start.TabIndex = 7;
+            this.btn_Start.Text = "Start";
+            this.btn_Start.UseVisualStyleBackColor = false;
+            this.btn_Start.Click += new System.EventHandler(this.btn_Start_Click);
+            // 
+            // btn_Stop
+            // 
+            this.btn_Stop.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btn_Stop.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btn_Stop.FlatAppearance.BorderSize = 0;
+            this.btn_Stop.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btn_Stop.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Stop.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Stop.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn_Stop.Location = new System.Drawing.Point(354, 318);
+            this.btn_Stop.Name = "btn_Stop";
+            this.btn_Stop.Size = new System.Drawing.Size(117, 47);
+            this.btn_Stop.TabIndex = 8;
+            this.btn_Stop.Text = "Stop";
+            this.btn_Stop.UseVisualStyleBackColor = false;
+            this.btn_Stop.Click += new System.EventHandler(this.btn_Stop_Click);
+            // 
+            // btn_Restart
+            // 
+            this.btn_Restart.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btn_Restart.FlatAppearance.BorderSize = 0;
+            this.btn_Restart.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(170)))), ((int)(((byte)(0)))));
+            this.btn_Restart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Restart.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Restart.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn_Restart.Location = new System.Drawing.Point(200, 318);
+            this.btn_Restart.Name = "btn_Restart";
+            this.btn_Restart.Size = new System.Drawing.Size(117, 47);
+            this.btn_Restart.TabIndex = 9;
+            this.btn_Restart.Text = "Restart";
+            this.btn_Restart.UseVisualStyleBackColor = false;
+            this.btn_Restart.Click += new System.EventHandler(this.btn_Restart_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(22)))), ((int)(((byte)(34)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_Restart);
+            this.Controls.Add(this.btn_Stop);
+            this.Controls.Add(this.btn_Start);
+            this.Controls.Add(this.lbl_Timer);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Timer";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbl_Sacend;
+        private System.Windows.Forms.Label lbl_Minute;
+        private System.Windows.Forms.Label lbl_Hour;
+        private System.Windows.Forms.Label lbl_Timer;
+        private System.Windows.Forms.Button btn_Start;
+        private System.Windows.Forms.Button btn_Stop;
+        private System.Windows.Forms.Button btn_Restart;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Timer.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Timer
+{
+    public partial class Timer : Form
+    {
+        public Timer()
+        {
+            InitializeComponent();
+        }
+
+        byte CounterSecend = 0;
+        byte CounterMinute = 0;
+        byte CounterHour   = 0;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            CounterSecend++;
+            lbl_Sacend.Text = CounterSecend.ToString();
+            lbl_Minute.Text = CounterMinute.ToString();
+            lbl_Hour.Text = CounterHour.ToString();
+
+            if (CounterSecend == 60)
+            {
+                CounterSecend = 0;
+                CounterMinute++;
+                if(CounterMinute == 60)
+                {
+                    CounterMinute= 0;
+                    CounterHour++;
+                }
+            }
+        }
+
+        private void btn_Start_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+        }
+
+        private void btn_Stop_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+        }
+
+        private void btn_Restart_Click(object sender, EventArgs e)
+        {
+           CounterHour = 0;
+           CounterMinute = 0;
+           CounterSecend = 0;
+            lbl_Hour.Text=CounterHour.ToString();
+            lbl_Minute.Text= CounterMinute.ToString();
+            lbl_Sacend.Text= CounterSecend.ToString();
+        }
+    }
+}
